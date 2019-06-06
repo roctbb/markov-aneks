f902560145659c55d2e3ace8878cf3105dba0492
roctbb <roctbb@gmail.com>
2019-06-06 13:10:00 +0300

    Initial commit

---
+++ generator.py
@@ -0,0 +1,24 @@
+import markovify
+import json
+from flask import Flask, render_template
+aneks = json.load(open('aneks.json'))
+
+text = ""
+
+for anek in aneks:
+    text += anek['text']
+
+m = markovify.Text(text)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    L = []
+    for i in range(5):
+        L.append(m.make_sentence().replace('\r\n', '<br>').replace('\n', '<br>'))
+    text = '<p>' + '</p><p>'.join(L) + '</p>'
+    return render_template('index.html', text=text)
+
+app.run(port='9095', host='0.0.0.0')
+
